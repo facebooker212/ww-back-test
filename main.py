efe58a53c18b5b51f1fd8ec386623795d06a2361
facebooker212 <facebooker3124@yahoo.com>
2023-04-28 16:31:29 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from bson import json_util
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask_cors import CORS
+import smtplib, ssl
+import os
+import sys
+import urllib
+import json
+import random
+import string
+import time
+
+app = Flask(__name__)  # Flask server
+CORS(app)
+
+client = MongoClient()  # Create client object
+
+load_dotenv()  # Load environment variables
+
+mongouri = "mongodb://" + urllib.parse.quote_plus(sys.argv[1]) + ":"\
+        + urllib.parse.quote_plus(sys.argv[2]) + "@127.0.0.1:27017/"
+client = MongoClient(mongouri)  # Client makes connection
+
+db = client['ww-test']  # Select database
+
+@app.route('/sales/repeater', methods=['POST'])
+def sales():
+    data = request.get_json()
+    cars = db.cars.find()
+    cars = json.loads(json_util.dumps(cars))
+    for i in cars:
+        i["_id"] = str(i["_id"]["$oid"])
+    return cars
+
+if __name__ == '__main__':
+    from waitress import serve
+    # For local testing
+    app.run(use_reloader=True, port=7000, threaded=True)
+    # For deployment
+    #tl.start()
+    #serve(app, host="0.0.0.0", port=5000, url_scheme='https')
